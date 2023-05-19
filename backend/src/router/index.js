b12c82b1739060ebf184e57a6273a0fad4e08f40
armanhn <arman.hn@gmail.com>
2023-05-19 18:09:43 +0600

    updated projects added login view with tailwindui

---
+++ backend/src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Dashboard from "../views/dashboard.vue";
+import Login from "../views/login.vue";
+
+const routes = [
+    {
+        path:'/dashboard',
+        name: 'dashboard',
+        component: Dashboard
+    },
+    {
+        path:'/login',
+        name: 'login',
+        component:Login
+    }
+];
+
+const router = createRouter({
+    history:createWebHistory(),
+    routes
+});
+
+export default router;
